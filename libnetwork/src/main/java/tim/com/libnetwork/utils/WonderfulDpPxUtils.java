1d50707a8c4a6acac98ee7a6310afaea84b6eb37
“timqi” <liuweiqitim@gmail.com>
2020-05-19 19:50:15 +0800

    add dp px util

---
+++ libnetwork/src/main/java/tim/com/libnetwork/utils/WonderfulDpPxUtils.java
@@ -0,0 +1,28 @@
+package tim.com.libnetwork.utils;
+
+import android.content.Context;
+
+/**
+ * ${description}
+ *
+ * @author weiqiliu
+ * @version 1.0 2020/5/19
+ */
+public class WonderfulDpPxUtils {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+}
