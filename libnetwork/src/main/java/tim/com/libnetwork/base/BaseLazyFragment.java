4ef1543feb1c68d54586eafc6d0a780d9bf1e87b
“timqi” <liuweiqitim@gmail.com>
2020-05-21 18:52:47 +0800

    add base lazy

---
+++ libnetwork/src/main/java/tim/com/libnetwork/base/BaseLazyFragment.java
@@ -0,0 +1,59 @@
+package tim.com.libnetwork.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+/**
+ * ${description}
+ *
+ * @author weiqiliu
+ * @version 1.0 2020/5/21
+ */
+public abstract class BaseLazyFragment extends BaseFragment {
+    /**
+     * 是否正在加载数据
+     */
+    protected boolean isLoad = false;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tryToLoadData();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        tryToLoadData();
+    }
+
+    private void tryToLoadData() {
+        if (!isInit) return;
+        if (!isNeedLoad) return;
+        if (getUserVisibleHint()) {
+            rootView.post(new Runnable() {
+                @Override
+                public void run() {
+                    loadData();
+                }
+            });
+            isLoad = true;
+        } else {
+            if (isLoad) stopLoad();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        isInit = false;
+        isLoad = false;
+        super.onDestroyView();
+    }
+
+    /**
+     * 隐藏时停止 加载数据
+     */
+    protected void stopLoad() {
+    }
+}
