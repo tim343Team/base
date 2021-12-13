3b022ee512d91900159f4be3f97ecfda9bddbf3e
“timqi” <liuweiqitim@gmail.com>
2021-12-13 14:29:53 +0800

    update bufferknife

--- app/src/main/java/tim/com/network/MainActivity.java
+++ app/src/main/java/tim/com/network/MainActivity.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import tim.com.libnetwork.utils.AnimationUtil;
+
 
 public class MainActivity extends AppCompatActivity {
+//    @BindView(R2.id.text)
+//    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+//        text.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this,"测试数据",Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
