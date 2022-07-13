56f6089ef36aabed42c22fd1d32e47c006b8d749
“timqi” <liuweiqitim@gmail.com>
2022-07-13 15:17:21 +0800

    add mailbox pattern

--- libnetwork/src/main/java/tim/com/libnetwork/utils/CharUtil.java
+++ libnetwork/src/main/java/tim/com/libnetwork/utils/CharUtil.java
@@ -1,5 +1,7 @@
 package tim.com.libnetwork.utils;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -13,6 +15,17 @@ public class CharUtil {
         public static Pattern MOBILE = Pattern.compile("^1(3|5|7|8)[0-9]{9}$");
         public static Pattern NUMBER = Pattern.compile("^1[0-9]{10}$");
         public static Pattern GRASS = Pattern.compile("[0-9|a-z|A-Z|:|_|(|)| |\\\\|/|\\||\\-|*|#]{4,20}");
+        public static Pattern EMAIL = Pattern.compile("[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+");
     }
 
+    public static boolean checkoutEmail(String email) {
+        boolean flag = false;
+        try {
+            Matcher matcher = Patterns.EMAIL.matcher(email);
+            flag = matcher.matches();
+        } catch (Exception e) {
+            flag = false;
+        }
+        return flag;
+    }
 }
